a533c1b17816ff74f05024808aeb2e958cc83b3a
xghos <xghos@DESKTOP-UU527UH>
2024-07-29 20:39:39 -0400

    Agregar archivos de proyecto.

---
+++ SolucionFachada/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolucionFachada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MortgageFacade mortgageFacade = new MortgageFacade();
+
+            Customer customer = new Customer("Ann McKinsey");
+            bool eligible = mortgageFacade.IsEligible(customer, 125000);
+
+            Console.WriteLine("\n" + customer.Name +
+                    " has been " + (eligible ? "Approved" : "Rejected"));
+
+            Console.ReadKey();
+        }
+    }
+  
+    public class Bank
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    public class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+    public class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+    public class Customer
+    {
+        private string name;
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+    public class MortgageFacade
+    {
+        private Bank bank;
+        private Loan loan;
+        private Credit credit;
+
+        public MortgageFacade()
+        {
+            bank = new Bank();
+            loan = new Loan();
+            credit = new Credit();
+        }
+
+        public bool IsEligible(Customer cust, int amount)
+        {
+            Console.WriteLine("{0} applies for {1:C} loan\n", cust.Name, amount);
+
+            bool eligible = true;
+
+            if (!bank.HasSufficientSavings(cust, amount))
+            {
+                eligible = false;
+            }
+            else if (!loan.HasNoBadLoans(cust))
+            {
+                eligible = false;
+            }
+            else if (!credit.HasGoodCredit(cust))
+            {
+                eligible = false;
+            }
+
+            return eligible;
+        }
+    }
+}
